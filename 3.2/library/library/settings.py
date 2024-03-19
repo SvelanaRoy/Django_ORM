d192c027ff150c381ef1ddf33e2c7940f7ff777d
Svetka <sveta@cronos.ru>
2024-03-19 19:51:44 +0300

    work

--- 3.2/library/library/settings.py
+++ 3.2/library/library/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'название_БД',
-        'USER': 'имя_пользователя',
-        'PASSWORD': 'пароль',
+        'NAME': 'testCRUD2',
+        'USER': 'postgres',
+        'PASSWORD': 'abc123',
         'HOST': 'localhost',
         'PORT': 5432
     }
